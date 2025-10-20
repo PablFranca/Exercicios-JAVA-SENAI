c4eb8554d4b7ac59e6b65da30191a958b55b4f7d
Pablo Lucas <66747830+PablFranca@users.noreply.github.com>
2025-10-20 19:19:05 -0300

    Add sound attribute and update methods in Animal class
    Added a new attribute 'somDoAnimal' to store the animal's sound and updated the constructor and methods to accommodate this change.

--- Polimorfismo/Animais/src/main/java/GruposDeAnimais/Animal.java
+++ Polimorfismo/Animais/src/main/java/GruposDeAnimais/Animal.java
@@ -6,15 +6,19 @@ public abstract class Animal {
     private GrupoAnimal grupo;
     private double pesoMedio;
     private double alturaMedia;
+    private String somDoAnimal;
 
-    public Animal(String nomePopular, String nomeCientifico, GrupoAnimal grupo, double peso, double altura){
+    public Animal(String nomePopular, String nomeCientifico, GrupoAnimal grupo, double peso, double altura, String somDoAnimal){
         this.nomePopular = nomePopular;
         this.nomeCientifico = nomeCientifico;
         this.grupo = grupo;
         this.pesoMedio= peso;
         this.alturaMedia =altura;
+        this.somDoAnimal
     }
-    public abstract void emitirSom();
+    public String emitirSom(){
+        return this.somDoAnimal;
+    }
 
     public void mostrarInformacoes(){
         System.out.println("O animal de nome popular " + nomePopular);
@@ -23,6 +27,7 @@ public abstract class Animal {
         System.out.println(" da familia dos " + grupo.toString().toLowerCase());
         System.out.println("seu tamanho médio é "+ alturaMedia);
         System.out.println("e a sua altura média é "+pesoMedio);
+        Sysyem.out.println(" O som que ele faz é "+ emitirSom() )
     }
 
     public String getNomePopular() {
@@ -65,3 +70,4 @@ public abstract class Animal {
         this.alturaMedia = altura;
     }
 }
+
