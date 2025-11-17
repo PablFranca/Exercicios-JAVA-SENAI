dffbea821362ebd99baf089038e3454ae5a4d5b6
Pablo Lucas <66747830+PablFranca@users.noreply.github.com>
2025-11-16 23:43:34 -0300

    Adição de exercicio lanches da tia

---
+++ lanches_da_tia/src/main/java/com/springapp/lanches_da_tia/LanchesDaTiaApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.lanches_da_tia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanchesDaTiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanchesDaTiaApplication.class, args);
+    }
+
+}
